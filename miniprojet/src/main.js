4b319f53cdcc81a8d1033af9ffdffeb3c19b2cf7
yoni_iut_nice <yoni.baroukh@hotmail.com>
2019-12-27 17:13:16 +0100

    add project router

--- miniprojet/src/main.js
+++ miniprojet/src/main.js
@@ -5,7 +5,7 @@ import App from './App.vue'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
-// import VueRouter from 'vue-router'
+
 
 Vue.config.productionTip = false
 Vue.use(VueMaterial)
