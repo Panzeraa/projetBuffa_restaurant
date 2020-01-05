185a2b1dd6b1743471116305018f96cabf7e5c64
Panzeraa <alexpanz06@gmail.com>
2020-01-05 22:52:12 +0100

    Update ajouter restaurant avec maps

--- projetbuffa_modele_cli_router/src/components/RestaurantAdd.vue
+++ projetbuffa_modele_cli_router/src/components/RestaurantAdd.vue
@@ -14,7 +14,8 @@
                                 <div class="md-layout-item md-small-size-100">
                                     <md-field>
                                         <label for="restaurant_name">Name</label>
-                                        <md-input name="restaurant_name" id="restaurant_name" v-model="form.name" />
+                                        <md-input name="restaurant_name" id="restaurant_name"
+                                            v-model="restaurantAdd.name" />
                                     </md-field>
                                 </div>
 
@@ -22,7 +23,38 @@
                                     <md-field>
                                         <label for="restaurant_cuisine">Cuisine</label>
                                         <md-input name="restaurant_cuisine" id="restaurant_cuisine"
-                                            v-model="form.cuisine" />
+                                            v-model="restaurantAdd.cuisine" />
+                                    </md-field>
+                                </div>
+                            </div>
+                            <div class="md-layout md-gutter">
+                                <div class="md-layout-item md-small-size-100">
+                                    <md-field>
+                                        <label for="restaurant_building">Numero</label>
+                                        <md-input name="restaurant_building" id="restaurant_building"
+                                            v-model="restaurantAdd.address.building" />
+                                    </md-field>
+                                </div>
+
+                                <div class="md-layout-item md-small-size-100">
+                                    <md-field>
+                                        <label for="restaurant_street">Adresse</label>
+                                        <md-input name="restaurant_street" id="restaurant_street"
+                                            v-model="restaurantAdd.address.street" />
+                                    </md-field>
+                                </div>
+                                <div class="md-layout-item md-small-size-100">
+                                    <md-field>
+                                        <label for="restaurant_zipcode">Code postale</label>
+                                        <md-input name="restaurant_zipcode" id="restaurant_zipcode"
+                                            v-model="restaurantAdd.address.zipcode" />
+                                    </md-field>
+                                </div>
+                                <div class="md-layout-item md-small-size-100">
+                                    <md-field>
+                                        <label for="restaurant_borough">Ville</label>
+                                        <md-input name="restaurant_borough" id="restaurant_borough"
+                                            v-model="restaurantAdd.borough" />
                                     </md-field>
                                 </div>
                             </div>
@@ -30,7 +62,8 @@
                                 <div class="md-layout-item md-small-size-100">
                                     <md-field>
                                         <label for="restaurant_latitude">Latitude</label>
-                                        <md-input name="restaurant_latitude" id="restaurant_latitude" v-model="latAdd" />
+                                        <md-input name="restaurant_latitude" id="restaurant_latitude"
+                                            v-model="restaurantAdd.address.lat" />
                                     </md-field>
                                 </div>
 
@@ -38,13 +71,16 @@
                                     <md-field>
                                         <label for="restaurant_longitudee">Longitude</label>
                                         <md-input name="restaurant_longitudee" id="restaurant_longitudee"
-                                            v-model="lonAdd" />
+                                            v-model="restaurantAdd.address.lon" />
                                     </md-field>
                                 </div>
                             </div>
                         </form>
                         <!-- lat : {{latAdd}}, lon : {{lonAdd}} -->
                     </md-card-content>
+                    <md-card-actions>
+                        <md-button>Ajouter (ne fonctionne pas)</md-button>
+                      </md-card-actions>
                 </md-card>
             </div>
             <div class="md-layout-item">
@@ -74,17 +110,19 @@
         data: function () {
             return {
                 L: null,
-                restaurant: {
+                restaurantAdd: {
                     name: null,
                     cuisine: null,
+                    borough: null,
                     address: {
                         building: null,
                         street: null,
-                        zipcode: null
+                        zipcode: null,
+                        lat: null,
+                        lon: null
                     }
                 },
-                latAdd: null,
-                lonAdd: null,
+                apiURLNominatim: "https://nominatim.openstreetmap.org/reverse?format=jsonv2&",
                 apiURL: "http://localhost:8081/api/restaurants",
                 mapAdd: null,
                 markerClick: null,
@@ -124,25 +162,55 @@
                 this.mapAdd.on('click', (e) => {
                     var pos = e.latlng;
                     this.displayMarker(e.latlng);
+                    this.getAdressefromLatLng(e.latlng);
                     console.log(pos);
                 });
             },
             displayMarker(latlng) {
                 if (this.mapAdd != null) {
-                    if(this.markerClick != null){
+                    if (this.markerClick != null) {
                         this.mapAdd.removeLayer(this.markerClick);
                     }
                     // this.map.setView([this.restaurant.address.coord[1], this.restaurant.address.coord[0]], 4);
                     this.markerClick = this.L.marker(latlng);
                     this.markerClick.addTo(this.mapAdd);
                     this.mapAdd.panTo(latlng);
-                    this.latAdd = latlng.lat;
-                    this.lonAdd = latlng.lng;
+                    this.restaurantAdd.address.lat = latlng.lat;
+                    this.restaurantAdd.address.lon = latlng.lng;
                 }
             },
             flyto() {
                 this.map.flyTo([this.restaurant.address.coord[1], this.restaurant.address.coord[0]], 17, { duration: 2, easeLinearity: 0.05 });
-            }
+            },
+            async getAdressefromLatLng(latlng) {
+                //this.restaurants.splice(index, 1);
+                try {
+                    let url = this.apiURLNominatim + "lat=" + latlng.lat + "&lon=" + latlng.lng;
+                    let reponseJSON = await fetch(url, {
+                        method: "GET"
+                    });
+                    let reponseJS = await reponseJSON.json();
+                    this.restaurantAdd.address.zipcode = reponseJS.address.postcode;
+
+                    this.restaurantAdd.address.building = reponseJS.address.house_number;
+
+                    if (reponseJS.address.city != null)
+                        this.restaurantAdd.borough = reponseJS.address.city;
+                    else if (reponseJS.address.town != null)
+                        this.restaurantAdd.borough = reponseJS.address.town;
+
+                    if (reponseJS.address.road != null)
+                        this.restaurantAdd.address.street = reponseJS.address.road;
+                    else if (reponseJS.address.pedestrian != null)
+                        this.restaurantAdd.address.street = reponseJS.address.pedestrian;
+
+                    if(reponseJS.address.restaurant != null)
+                        this.restaurantAdd.name = reponseJS.address.restaurant;
+                    console.log(reponseJS);
+                } catch (err) {
+                    console.log("Erreur dans le fetchs get Nominatim " + err.msg);
+                }
+            },
         }
     };
 </script>
